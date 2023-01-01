49e0000c27bde37096bd485dc3edff7d1185648e
Héctor Manuel Sandoval Landázuri <ingsan@live.com.mx>
2023-01-01 08:59:52 -0600

    Agregar archivos de proyecto.

---
+++ InheritanceDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+Car car =new Car();
+car.NumberOfDoors = 4;
+car.NumberOfWheels = 4;
+
+
+
+public class Car
+{
+    public int NumberOfWheels { get; set; }
+    public int NumberOfDoors { get; set; }
+
+    public void StartCar()
+    {
+
+    }
+
+    public void StopCar()
+    {
+
+    }
+}
+
+public class Corolla
+{
+
+}
